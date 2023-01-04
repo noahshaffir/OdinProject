6653e63db33a1e7b7a22e6f40c9c2c927702effb
noahshaffir <noahshaffir@gmail.com>
2023-01-04 00:29:51 -0500

    good progress

---
+++ api.js
@@ -0,0 +1,10 @@
+
+  let img = document.querySelector('img');
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats', {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .then(alert(response));
