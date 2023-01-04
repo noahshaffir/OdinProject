b7928fd18cbfa314a992ec2c9c266e6bcc4a252e
noahshaffir <noahshaffir@gmail.com>
2023-01-03 19:57:56 -0500

    working thru

--- app.js
+++ app.js
@@ -31,3 +31,10 @@ container.appendChild(content);
 container.appendChild(redP);
 container.appendChild(h3);
 container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.onclick=()=>{alert("Hello World 2")};
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert("Hello World 3");
+});
